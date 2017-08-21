a8905f028c4cfc0e07b39a949e84c9f5f3d63120
dmitryg <dmitryg@hexagontech.co.il>
2017-08-21 19:15:13 +0300

    map navigation bug

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,36 +1,56 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
+
 export class Map extends React.Component {
 	constructor(props) {
 		super(props);
-	}
-
-	componentWillUpdate() {
-		console.log("will update..");
-		this.setUserLocation();
+		 this.state = {
+			 points: []
+			};
 	}
 
 	componentDidMount() {
 		console.log("mounted..", this.map);
 		DG.then(() => {
-			this.map = DG.map('map').on('click', function (e) {
-				console.log('карту, координаты ' + e.latlng.lat + ', ' + e.latlng.lng);
-				DG.marker([e.latlng.lat, e.latlng.lng]).addTo(this);
-			});
+			this.map = DG.map('map').on('click', (e) => this.clickHandler(e));
 		})
-		.then(() => this.setUserLocation());
+		.then(() => this.setUserLocation())
+		.then(() => this.renderMarkers());
+	}
+
+	clickHandler(e) {
+		let marker = {lat: e.latlng.lat, lng: e.latlng.lng};
+		DG.marker([e.latlng.lat, e.latlng.lng]).addTo(this.map);
+		this.addMarker(marker);
+	}
+
+	addMarker(marker) {
+		let copyState = {
+			points: this.state.points.slice()
+		};
+		copyState.points.push(marker);
+		this.setState(copyState);
+		let stateString = JSON.stringify(copyState);
+		localStorage.setItem('state', stateString);
+	}
+
+	renderMarkers() {
+		let state = JSON.parse(localStorage.getItem('state'));
+		state.points.forEach((point) => {
+			DG.marker([point.lat, point.lng]).addTo(this.map);
+		});
 	}
 
 	setUserLocation() {
-		console.log("Map", this.map);
 		this.map.locate({
 			setView: true,
 			watch: true
 		}).on('locationfound', function (e) {
-			console.log(e);
+			console.info(e);
 			DG.marker([e.latitude, e.longitude]).addTo(this).bindPopup('Вы кликнули по мне!');
 		}).on('locationerror', function (e) {
+			console.error(e);
 			DG.popup()
 				.setLatLng(this.getCenter())
 				.setContent('Доступ к определению местоположения отключён')
