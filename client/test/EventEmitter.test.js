9a18a5d457a5d1a790aa4d206c15d7424db5b03e
dmitryg <dmitryg@hexagontech.co.il>
2017-08-30 12:19:02 +0300

    tests added

--- client/test/EventEmitter.test.js
+++ client/test/EventEmitter.test.js
@@ -1,7 +1,32 @@
+import assert from 'assert';
+import { expect, should } from "chai";
 import EventEmitter from '../src/services/EventEmitter';
 
-describe('EventEmitter', function(){
-  it('should subscribe for events', function(){
-    
-  })
+describe('EventEmitter', () => {
+	let service;
+	beforeEach(() => {
+		service = new EventEmitter(); 
+  	});
+
+	describe('#basic API tests', () => {
+		it('should subscribe for events', () => {
+			return false;
+		});
+
+		it('should unsubscribe for events', () => {
+
+  		});
+
+		it('should subscribe for one event', () => {
+			[1,2,3].indexOf(0).should.be.equal(-1);
+  		});
+		
+		it('should emit an event', () => {
+			
+  		});
+
+		it('should return listeners for a specific event', () => {
+			
+  		});
+	});  
 });
